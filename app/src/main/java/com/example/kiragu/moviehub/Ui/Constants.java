098d079c7891794d4027cc5c74b450331031c14f
Gathua Kiragu <kiragugathua@gmail.com>
2017-09-04 16:23:52 +0300

    Initializing String constants

--- app/src/main/java/com/example/kiragu/moviehub/Ui/Constants.java
+++ app/src/main/java/com/example/kiragu/moviehub/Ui/Constants.java
@@ -8,4 +8,14 @@ import com.example.kiragu.moviehub.BuildConfig;
 
 public class Constants {
     public static final String THE_MOVIE_Db_Api = BuildConfig.MOVIE_Api;
+    public static final String LATEST__MOVIES_BASE_URL = "https://api.themoviedb.org/4/list/1?api_key=";
+    public static final String GENRE_BASE_URL = "https://api.themoviedb.org/3/genre/movie/list?api_key=";
+
+
+//    https://api.themoviedb.org/3/search/movie?api_key={{Api_key}}&language=en-US&query=Naked&page=1
+    public static final String SEARCH_BASE_URL = "https://api.themoviedb.org/3/search/movie?api_key=";
+    public static final String SEARCH_BASE_ADDITION_URL = "&language=en-US&query=";
+    public static final String SEARCH_QUERY ="query";
+    public static final String FINAL_SEARCH_BASE_URL = "&page=1";
+
 }
