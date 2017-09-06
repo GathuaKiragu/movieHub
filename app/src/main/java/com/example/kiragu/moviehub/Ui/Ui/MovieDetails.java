ebbe0b5be93d208b8b99917bbc52b185a67c012f
Gathua Kiragu <kiragugathua@gmail.com>
2017-09-06 15:18:13 +0300

    Added parceler library to allow the serialization and decerialization of movieObjects.

--- app/src/main/java/com/example/kiragu/moviehub/Ui/Ui/MovieDetails.java
+++ app/src/main/java/com/example/kiragu/moviehub/Ui/Ui/MovieDetails.java
@@ -1,9 +1,13 @@
 package com.example.kiragu.moviehub.Ui.Ui;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.kiragu.moviehub.R;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v13.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v13.FragmentPagerItems;
 
 public class MovieDetails extends AppCompatActivity {
 
@@ -11,5 +15,17 @@ public class MovieDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+//        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+//                getSupportFragmentManager(), FragmentPagerItems.with(this)
+//                .add(R.string.titleA, PageFragment.class)
+//                .add(R.string.titleB, PageFragment.class)
+//                .create());
+//
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+//        viewPager.setAdapter(adapter);
+//
+//        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+//        viewPagerTab.setViewPager(viewPager);
     }
 }
