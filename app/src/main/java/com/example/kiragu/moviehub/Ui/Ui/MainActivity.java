00266fa82e02c4ce19b0dc37d13c2d844d902c3b
Gathua Kiragu <kiragugathua@gmail.com>
2017-09-08 15:10:26 +0300

    Added a search widget

--- app/src/main/java/com/example/kiragu/moviehub/Ui/Ui/MainActivity.java
+++ app/src/main/java/com/example/kiragu/moviehub/Ui/Ui/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.kiragu.moviehub.Ui.Ui;
 
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -90,6 +95,16 @@ public void onClick(View v){
         mSlider.stopAutoCycle();
         super.onStop();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+
+        return true;
+    }
+
+
 
 
 
