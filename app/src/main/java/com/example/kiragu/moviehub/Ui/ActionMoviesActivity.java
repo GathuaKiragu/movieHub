fc14f507e29ec017856d75f523d16811ce145731
Gathua Kiragu <kiragugathua@gmail.com>
2017-09-12 10:22:46 +0300

    Tested click listeners

---
+++ app/src/main/java/com/example/kiragu/moviehub/Ui/ActionMoviesActivity.java
@@ -0,0 +1,15 @@
+package com.example.kiragu.moviehub.Ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kiragu.moviehub.R;
+
+public class ActionMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_movies);
+    }
+}
--- app/src/main/java/com/example/kiragu/moviehub/Ui/Ui/MainActivity.java
+++ app/src/main/java/com/example/kiragu/moviehub/Ui/Ui/MainActivity.java
@@ -22,11 +22,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daimajia.slider.library.Indicators.PagerIndicator;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.example.kiragu.moviehub.Ui.ActionMoviesActivity;
 import com.example.kiragu.moviehub.Ui.theMovieDbService;
 import com.example.kiragu.moviehub.R;
 
@@ -40,70 +42,73 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity{
-    public static final String TAG = MovieListActivity.class.getSimpleName();
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public static final String TAG=MovieListActivity.class.getSimpleName();
 
-    @Bind(R.id.slider)
+@Bind(R.id.slider)
     SliderLayout mSlider;
 
 
 // Navigation Drawer
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
+private DrawerLayout mDrawerLayout;
+private ActionBarDrawerToggle mToggle;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+@Override
+protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        HashMap<String,String> url_maps = new HashMap<String, String>();
-        url_maps.put("The Fast and Furious 8", "https://www.bellanaija.com/wp-content/uploads/2017/04/pd.jpg");
-        url_maps.put("The Boss Baby", "https://i.ytimg.com/vi/tquIfapGVqs/maxresdefault.jpg");
-        url_maps.put("Despicable me 3", "https://thebaytheatre.com/wp-content/uploads/2017/06/depicable-me-3-1496947425.jpg");
-        url_maps.put("The Mummy", "http://www.dhakamovie.com/wp-content/uploads/2017/07/The-Mummy-2017-1080p-WEBRip.jpg");
+        HashMap<String, String> url_maps=new HashMap<String, String>();
+        url_maps.put("The Fast and Furious 8","https://www.bellanaija.com/wp-content/uploads/2017/04/pd.jpg");
+        url_maps.put("The Boss Baby","https://i.ytimg.com/vi/tquIfapGVqs/maxresdefault.jpg");
+        url_maps.put("Despicable me 3","https://thebaytheatre.com/wp-content/uploads/2017/06/depicable-me-3-1496947425.jpg");
+        url_maps.put("The Mummy","http://www.dhakamovie.com/wp-content/uploads/2017/07/The-Mummy-2017-1080p-WEBRip.jpg");
 
 
-        for(String name : url_maps.keySet()){
-            TextSliderView textSliderView = new TextSliderView(this);
-            // initialize a SliderLayout
-            textSliderView
-                    .description(name)
-                    .image(url_maps.get(name))
-                    .setScaleType(BaseSliderView.ScaleType.Fit);
+        for(String name:url_maps.keySet()){
+        TextSliderView textSliderView=new TextSliderView(this);
+        // initialize a SliderLayout
+        textSliderView
+        .description(name)
+        .image(url_maps.get(name))
+        .setScaleType(BaseSliderView.ScaleType.Fit);
 
-            //add your extra information
-            textSliderView.bundle(new Bundle());
-            textSliderView.getBundle()
-                    .putString("extra",name);
+        //add your extra information
+        textSliderView.bundle(new Bundle());
+        textSliderView.getBundle()
+        .putString("extra",name);
 
-            mSlider.addSlider(textSliderView);
+        mSlider.addSlider(textSliderView);
 
-            Intent intent = getIntent();
-            String query = intent.getStringExtra("movies");
+        Intent intent=getIntent();
+        String query=intent.getStringExtra("movies");
         }
-
 
 
 //  Navigation
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        mToggle=new ActionBarDrawerToggle(this,mDrawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mToggle.onOptionsItemSelected(item)) {
-            return true;
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        }
+
+@Override
+public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+        return true;
         }
         return super.onOptionsItemSelected(item);
 
-    }
+        }
+
 
 //Using intents to pass the typed search query to MovieListActivity
 
@@ -116,38 +121,52 @@ public class MainActivity extends AppCompatActivity{
 //}
 
 //    To prevent a memory leak on rotation, \we call stopAutoCycle() on the slider before activity or fragment is destroyed:
-    @Override
-    protected void onStop(){
+@Override
+protected void onStop(){
         mSlider.stopAutoCycle();
         super.onStop();
-    }
+        }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.option_menu, menu);
-        final MenuItem menuItem = menu.findItem(R.id.search);
+@Override
+public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.option_menu,menu);
+final MenuItem menuItem=menu.findItem(R.id.search);
 
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        SearchView searchView=(SearchView)MenuItemCompat.getActionView(menuItem);
 
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
 
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                Intent intent = new Intent(MainActivity.this, MovieListActivity.class);
-                intent.putExtra("query", query);
-                startActivity(intent);
-                return false;
-            }
+@Override
+public boolean onQueryTextSubmit(String query){
+        Intent intent=new Intent(MainActivity.this,MovieListActivity.class);
+        intent.putExtra("query",query);
+        startActivity(intent);
+        return false;
+        }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
+@Override
+public boolean onQueryTextChange(String newText){
+        return false;
+        }
 
         });
         return true;
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item){
+            int id = item.getItemId();
+
+            if (id == R.id.nav_action){
+                    Intent myIntent = new Intent(MainActivity.this, ActionMoviesActivity.class);
+                    MainActivity.this.startActivity(myIntent);
+            }else{
+                    Toast.makeText(getApplicationContext(), "Working.", Toast.LENGTH_LONG).show();
+            }
+            return true;
     }
 
 }
